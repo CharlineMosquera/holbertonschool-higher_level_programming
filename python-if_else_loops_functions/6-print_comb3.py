88ed8e11bc42339566a5cbb9d80f7a7da4e3a6be
CharlineMosquera <char951144@gmail.com>
2023-01-25 20:24:20 -0500

    Print comb3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for dig_1 in range(0, 10):
     for dig_2 in range(dig_1+1, 10):
         if dig_1 == 8:
-            print(f"{dig_1}{dig_2}")
+            print("{}{}".format(dig_1, dig_2))
         else:
-            print(f"{dig_1}{dig_2}", end=", ")
+            print("{}{}".format(dig_1, dig_2), end=", ")
