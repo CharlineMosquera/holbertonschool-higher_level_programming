25cb810772702e5221dc72594ace76ac0c19bb30
CharlineMosquera <char951144@gmail.com>
2023-02-03 00:53:19 -0500

    Print Matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for fila in matrix:
-        for value in fila:
-            if value % 3 == 0:
-                print("{:d}".format(value))
-            else:
-                print("{:d}".format(value), end=' ')
+    for fila in range(len(matrix)):
+        for value in range(len(matrix[fila])):
+            print("{:d}".format(matrix[fila][value]), end="")
+            if (value < len(matrix[fila]) - 1):
+                print(" ", end="")
+        print("")
