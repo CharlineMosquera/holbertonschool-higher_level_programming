6ebaebab5223608e5efb07f0310c2c993e0f3762
CharlineMosquera <char951144@gmail.com>
2023-02-09 15:17:21 -0500

    update file 5 square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -5,11 +5,7 @@
 class Square():
     """class Square"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     def area(self):
         return (self.__size ** 2)
