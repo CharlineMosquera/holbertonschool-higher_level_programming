8b0f7d951b7f00076e498662581658e4ff1acc53
CharlineMosquera <char951144@gmail.com>
2023-02-10 23:33:57 -0500

    update file 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 """function that prints a text"""
 
+
 def text_indentation(text):
     """Prints the text with two new lines after
     each of the following characters: ., ? and :"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     else:
-        text = text.strip()
+        prev = 0
         punctuation = [".", "?", ":"]
-        for char in text:
-            print(char, end="")
-            if char in punctuation:
-                print("\n" * 2, end="")
+        for i in range(len(text)):
+            if i == len(text) - 1:
+                print(text[prev:i + 1], end="")
+            elif text[i] in punctuation:
+                print(text[prev:i + 1], end="")
+                print('\n')
+                prev = i + 1
+                while text[prev] == " ":
+                    prev += 1
