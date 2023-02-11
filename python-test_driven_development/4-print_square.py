0138363d20486d83a120b2354645887f6d204feb
CharlineMosquera <char951144@gmail.com>
2023-02-10 23:07:50 -0500

    add file 4 print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -11,7 +11,7 @@ def print_square(size):
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     if size == 0:
-        print("")
+        return
     for i in range(1, size + 1):
         for j in range(1, size + 1):
             print("#", end="")
