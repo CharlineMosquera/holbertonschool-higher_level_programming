e8b976bb6d844dba28ff6bc5b7c868e5ac3e7ca9
CharlineMosquera <char951144@gmail.com>
2023-02-10 21:23:47 -0500

    update file 0 add integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Function that adds 2 integers."""
 
+
 def add_integer(a, b=98):
-    """Adds two integers or floating-point numbers and returns the result as an integer."""
+    """Adds two integers or floating-point
+    numbers and returns the result as an integer."""
     if type(a) == float:
         a = int(a)
     if type(b) == float:
