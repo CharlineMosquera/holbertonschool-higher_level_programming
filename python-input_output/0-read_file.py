afcd4032a644c1f2503c0186833a67ae26b8d13a
CharlineMosquera <char951144@gmail.com>
2023-02-17 18:00:24 -0500

    file 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that reads a file"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
