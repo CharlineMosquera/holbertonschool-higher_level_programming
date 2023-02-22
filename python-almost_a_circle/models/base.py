0354ad66de15bf7696c14c695775612bb9110d77
CharlineMosquera <char951144@gmail.com>
2023-02-22 01:13:18 -0500

    staticmethod Dictionary to JSON string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module class Base"""
+import json
 
 
 class Base():
@@ -14,3 +15,11 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """static method returns JSON string
+        representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
