b1e985352b83f9b085edad5849f018981dc3759a
CharlineMosquera <char951144@gmail.com>
2023-02-22 00:27:07 -0500

    implementation method dictionary in class Square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -31,3 +31,10 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """returns dictionary with instance attributes"""
+        return {'id': self.id,
+                'size': self.size,
+                'x': self.x,
+                'y': self.y}
