a14751b659ffe07dc82e0a5430d6e68a454b7fca
CharlineMosquera <char951144@gmail.com>
2023-02-21 23:50:11 -0500

    public getter and setter methods for size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 """Module Square that inherits from Rectangle"""
-from models.rectangle import Rectangle
+from models.rectangle import Rectangle, validate_attributes
 
 
 class Square(Rectangle):
     """class Square"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        validate_attributes(value, "width")
+        self.__width = value
+        self.__height = value
 
     def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
