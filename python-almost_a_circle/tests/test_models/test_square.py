0faed703bbed333b2b95c1a2d136423bd1e0bf8b
CharlineMosquera <char951144@gmail.com>
2023-02-24 16:41:09 -0500

    update test square

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -8,89 +8,95 @@ from models.base import Base
 from models.square import Square
 
 
+
+
 class TestSquare(unittest.TestCase):
-    """These tests ensure that the Rectangle class behaves
+    """These tests ensure that the Square class behaves
     correctly and has the expected functionality."""
 
-    @classmethod
-    def setUpClass(cls):
-        """set up the tests"""
-        Base._Base__nb_objects = 0
-        cls.s1 = Square(1)
-        cls.s2 = Square(2, 3)
+    def test_square_exists(self):
+        s = Square(1)
+        self.assertIsInstance(s, Square)
 
-    def test_id(self):
-        """Test for functioning ID"""
-        self.assertEqual(self.s1.id, 1)
-        self.assertEqual(self.s2.id, 2)
+    def test_square_with_two_arguments_exists(self):
+        s = Square(1, 2)
+        self.assertIsInstance(s, Square)
 
-    def test_size(self):
-        """Test for functioning size"""
-        self.assertEqual(self.s1.size, 1)
-        self.assertEqual(self.s2.size, 2)
+    def test_square_with_three_arguments_exists(self):
+        s = Square(1, 2, 3)
+        self.assertIsInstance(s, Square)
 
-    def test_width(self):
-        self.assertEqual(self.s1.width, 1)
-        self.assertEqual(self.s2.width, 2)
+    def test_square_with_one_argument_string_exists(self):
+        s = Square("1")
+        self.assertIsInstance(s, Square)
 
-    def test_height(self):
-        """Test for functioning height"""
-        self.assertEqual(self.s1.height, 1)
-        self.assertEqual(self.s2.height, 2)
+    def test_square_with_two_arguments_one_string_exists(self):
+        s = Square(1, "2")
+        self.assertIsInstance(s, Square)
 
-    def test_x(self):
-        """Test for functioning x"""
-        self.assertEqual(self.s1.x, 0)
-        self.assertEqual(self.s2.x, 3)
+    def test_square_with_three_arguments_one_string_exists(self):
+        s = Square(1, 2, "3")
+        self.assertIsInstance(s, Square)
 
-    def test_y(self):
-        """Test for functioning y"""
-        self.assertEqual(self.s1.y, 0)
-        self.assertEqual(self.s2.y, 0)
+    def test_square_with_four_arguments_exists(self):
+        s = Square(1, 2, 3, 4)
+        self.assertIsInstance(s, Square)
 
-    def mandatory_size(self):
-        """Test that width is a mandatory arg"""
-        with self.assertRaises(TypeError):
-            s = Square()
+    def test_square_with_negative_size_exists(self):
+        s = Square(-1)
+        self.assertIsInstance(s, Square)
 
-    def size_typeerror(self):
-        """Test non-ints for size"""
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            s = Square("hello")
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            s = Square(True)
+    def test_square_with_negative_x_exists(self):
+        s = Square(1, -2)
+        self.assertIsInstance(s, Square)
 
-    def test_size_valueerror(self):
-        """Test ints <= 0 for size"""
-        with self.assertRaisesRegex(ValueError, "width must be > 0"):
-            s = Square(-1)
-        with self.assertRaisesRegex(ValueError, "width must be > 0"):
-            s = Square(0)
+    def test_square_with_negative_y_exists(self):
+        s = Square(1, 2, -3)
+        self.assertIsInstance(s, Square)
 
-    def test_x_valueerror(self):
-        """Test ints < 0 for x"""
-        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
-            s = Square(1, -1)
+    def test_square_with_zero_size_exists(self):
+        s = Square(0)
+        self.assertIsInstance(s, Square)
 
-    def test_y_valueerror(self):
-        """Test ints <= 0 for y"""
-        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
-            s = Square(1, 1, -1)
+    def test_square_str_exists(self):
+        s = Square(1)
+        self.assertEqual(str(s), "[Square] (1) 0/0 - 1")
 
-    def test_area(self):
-        """test area"""
-        self.assertEqual(self.s1.area(), 1)
-        self.assertEqual(self.s2.area(), 4)
+    def test_square_to_dictionary_exists(self):
+        s = Square(1)
+        d = s.to_dictionary()
+        expected_dict = {'id': 1, 'size': 1, 'x': 0, 'y': 0}
+        self.assertDictEqual(d, expected_dict)
 
-    def test_area_args(self):
-        """Test too many args for area()"""
-        with self.assertRaises(TypeError):
-            a = self.s1.area(1)
+    def test_square_update_exists(self):
+        s = Square(1)
+        s.update(2)
+        self.assertEqual(str(s), "[Square] (2) 0/0 - 1")
 
-    def test_display_too_many_args(self):
-        """Test display with too many args"""
-        with self.assertRaises(TypeError):
-            self.s1.display(1)
+    def test_square_update_with_one_argument_exists(self):
+        s = Square(1)
+        s.update(89)
+        self.assertEqual(str(s), "[Square] (89) 0/0 - 1")
+
+    def test_square_update_with_two_arguments_exists(self):
+        s = Square(1)
+        s.update(89, 1)
+        self.assertEqual(str(s), "[Square] (89) 0/0 - 1")
+
+    def test_square_update_with_three_arguments_exists(self):
+        s = Square(1)
+        s.update(89, 1, 2)
+        self.assertEqual(str(s), "[Square] (89) 2/0 - 1")
+
+    def test_square_update_with_four_arguments_exists(self):
+        s = Square(1)
+        s.update(89, 1, 2, 3)
+        self.assertEqual(str(s), "[Square] (89) 2/3 - 1")
+
+    def test_square_update_with_dictionary_exists(self):
+        s = Square(1)
+        s.update(**{'id': 89})
+        self.assertEqual(str(s), "[Square] (89) 0/0 - 1")
 
 if __name__ == '__main__':
     unittest.main()
