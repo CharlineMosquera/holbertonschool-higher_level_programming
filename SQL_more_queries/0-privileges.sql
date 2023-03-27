b60a28f78fe8736d0e68c6af1ffd255fb43ed7aa
CharlineMosquera <char951144@gmail.com>
2023-03-26 20:11:05 -0500

    list user privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,2 @@
 -- list user privileges
-SELECT * FROM information_schema.user_privileges 
-WHERE grantee IN ('user_0d_1'@'localhost', 'user_0d_2'@'localhost');
+SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
