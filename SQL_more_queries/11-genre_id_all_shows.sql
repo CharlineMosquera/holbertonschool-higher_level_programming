8e4857e3f97db087745ea13a626607b5a7d94863
CharlineMosquera <char951144@gmail.com>
2023-03-26 21:24:54 -0500

    lists all shows contained in the database hbtn_0d_tvshows

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
--- Import the database dump of hbtn_0d_tvshows
+-- lists all shows contained in the database hbtn_0d_tvshows
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
        LEFT JOIN `tv_show_genres` AS g
