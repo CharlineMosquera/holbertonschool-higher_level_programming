5aa13a22d9c17749e2adf73bc2919fb90e9a28d4
CharlineMosquera <char951144@gmail.com>
2023-09-11 18:36:13 -0500

    Add `.red` class

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,7 @@
+var redHeader = document.getElementById('red_header');
+
+redHeader.addEventListener('click', function () {
+    var header = document.querySelector('header');
+
+    header.classList.add('red');
+});
