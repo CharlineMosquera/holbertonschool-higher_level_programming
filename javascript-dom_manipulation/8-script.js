e534d0f0fefb746b7981d259d0b67e1ab2cfac84
CharlineMosquera <char951144@gmail.com>
2023-09-11 21:22:24 -0500

    Displays the value of an element's yield

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,15 @@
+var apiUrl = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+
+fetch(apiUrl)
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Unable to obtain translation');
+    })
+    .then(function (data) {
+        hello.textContent = data.hello
+    })
+    .catch(function (error) {
+        console.error('Error: ' + error.message);
+    });
